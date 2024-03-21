5a1805a6027612d1b9a60c540faa0a035f194e25
EDUARDO GONÇALVES DE OLIVEIRA <23111718@dsn127m1242314.cfp127aluno.local>
2024-03-21 16:33:16 -0300

    ListComplete

---
+++ app/src/main/java/com/example/listamusicas/MainActivity.kt
@@ -0,0 +1,192 @@
+package com.example.listamusicas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.listamusicas.ui.theme.ListaMusicasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ListaMusicasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Image(
+        painter = painterResource(id = R.drawable.showsupercombo),
+        contentDescription = "Background da Aplicaçao",
+        contentScale = ContentScale.FillHeight
+    )
+
+    Surface(color = Color(0x88000000)) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            OutlinedTextField(modifier = Modifier
+                .fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = Color(0xffffffff),
+                    focusedBorderColor = Color(0xffffffff),
+                    focusedTextColor = Color(0xffffffff),
+                    unfocusedTextColor = Color(0xffffffff),
+                    unfocusedLabelColor = Color(0xffffffff),
+                    focusedLabelColor = Color(0xffffffff),
+                    unfocusedContainerColor = Color(0x21ffffff)
+                ),
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text(text = "Buscar Músicas...")
+                },
+                shape = RoundedCornerShape(16.dp),
+                trailingIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Botão Buscar",
+                            tint = Color(0xffffffff)
+                        )
+                    }
+                })
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                text = "Personagens", color = Color(0xffffffff), fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+
+            LazyColumn() {
+
+                items(20) {
+
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(126.dp)
+                            .padding(bottom = 12.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color(0x8fffffff)
+                        ),
+                        border = BorderStroke(
+                            width = 2.dp, brush = Brush.horizontalGradient(
+                                listOf(
+                                    Color.Blue, Color.Blue, Color.Gray, Color.DarkGray, Color.Black
+                                )
+                            )
+                        )
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .fillMaxSize(),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Card(
+                                shape = RoundedCornerShape(8.dp)
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.selvagensaprocuradelei),
+                                    contentDescription = "Imagem De Banda"
+                                )
+
+                            }
+                            Column(
+                                modifier = Modifier
+                                    .padding(start = 6.dp, end = 8.dp)
+                            ) {
+                                Text(
+                                    text = "Selvagens à Procura de Lei", fontSize = 18.sp,
+                                    fontWeight = FontWeight.SemiBold,
+                                    color = Color(0xff000000)
+                                )
+                                Spacer(
+                                    modifier = Modifier
+                                        .height(6.dp)
+                                )
+                                Text(
+                                    text = "Álbum: Selvagens à Procura de Lei", fontSize = 12.sp,
+                                    fontWeight = FontWeight.Normal,
+                                    color = Color(0xff000000)
+                                )
+                            }
+
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    imageVector = Icons.Default.CheckCircle,
+                                    contentDescription = "Botão Buscar",
+                                    tint = Color.Green
+                                )
+                            }
+
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    ListaMusicasTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting("")
+        }
+    }
+}
